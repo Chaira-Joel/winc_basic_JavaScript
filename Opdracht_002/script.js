a4f1bb1960b30b9a1662883bd2e84fd7dfb1a30b
Chaïra Joel <79043671+Chaira-Joel@users.noreply.github.com>
2021-04-05 21:27:33 +0200

    Add files via upload

---
+++ Opdracht_002/script.js
@@ -0,0 +1,10 @@
+console.log('Hallo Winc Academy studenten');
+
+// Dit is een grote som
+console.log(1230941 * 1823794);
+
+// //deel 2 som 2
+console.log(637263 / 54);
+
+// nee ik zie de uitkomst van de comment niet omdat het gecomment is
+
