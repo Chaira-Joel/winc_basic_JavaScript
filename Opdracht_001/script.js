0fae0efdcea7001ff2835bf02691679fcb448428
Chaïra Joel <79043671+Chaira-Joel@users.noreply.github.com>
2021-04-05 17:33:56 +0200

    Add files via upload

---
+++ Opdracht_001/script.js
@@ -0,0 +1,28 @@
+
+
+console.log("Hello Winc Academy")
+
+//Opdracht 5
+let firstName=('Chaira');
+console.log(firstName);
+
+//Opdracht 6 Nee een variabel maken met if kan niet. Dit is geen bruikbare variable want het kan als if statement gebruikt worden
+
+//Opdracht 7
+console.log(4+4);
+
+//Strings optellen lukt niet, hij zet ze naast elkaar net als letters. Die zijn ook niet op te tellen
+
+//Opdracht 8
+console.log(33+33);
+console.log(3%55);
+console.log(5*5);
+console.log(35/851);
+
+//Opdracht 9
+let age=(34);
+console.log(typeof age);
+
+//opdracht 10
+let ageNew=('34');
+console.log(typeof ageNew);
