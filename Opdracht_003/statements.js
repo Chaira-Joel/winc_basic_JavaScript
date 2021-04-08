19b2626c936ecd7e705c1f7cbd39a173faf071ad
Chaïra Joel <79043671+Chaira-Joel@users.noreply.github.com>
2021-04-08 21:15:48 +0200

    Add files via upload

---
+++ Opdracht_003/statements.js
@@ -0,0 +1,33 @@
+
+//deel 1
+const age=18;
+
+if (age == 18){
+    console.log ('Please enter')
+}
+
+else if (age > 18) {
+    console.log('Please enter')
+}
+
+else {
+    console.log('Get out of here')
+}
+
+//deel 2
+const isFemale= true;
+
+if (isFemale){
+    console.log('Je bent een vrouw')
+} 
+
+//deel 3
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob'){
+    console.log('jij mag rijden')
+}
+
+else {
+    console.log('nee, jij mag niet rijden')
+}
