3aaa43c71404ed0a06d4dc60a1a5252760bf0fd6
Chaïra Joel <79043671+Chaira-Joel@users.noreply.github.com>
2021-04-18 15:52:44 +0200

    Add files via upload

---
+++ Opdracht_006/functions.js
@@ -0,0 +1,33 @@
+//pizza function
+
+const makePizza = function (){
+    console.log("Maak het deeg");
+    console.log("Zet het deeg in de koelkast en laat het rijzen");
+    console.log("maak de tomatensaus");
+    console.log("snijd de paprika, pepperoni en champignons.");
+    console.log("rasp de kaas");
+    console.log("zet de oven aan op 220 graden celcius");
+    console.log("Haal het deeg uit de koelkast en maak er een platte schijf van");
+    console.log("Breng met de bolle kant van de lepel de saus aan op het deeg");
+    console.log("Beleg het deeg mt kaas, paprika, pepperoni en de champignons");
+    console.log("Zet de pizza in de oven en bak hem 30 minuten");
+    console.log("Haal de pizza na 30 minuten uit de oven en eet hem op");
+}
+// makePizza();
+// makePizza();
+// makePizza();
+
+const makeSushi = function(){
+    console.log("Snijd de zalm en de tonijn in kleine stukken");
+    console.log("Kook de rijst 10 minuten");
+    console.log("Rol de rijst met de tonijn op met het zeewier");
+    console.log("Rol de rijst met de zalm op met het zeewier");
+    console.log("Zet de rolletjes op een plaat");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
