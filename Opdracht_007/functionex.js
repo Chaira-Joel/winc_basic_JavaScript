efa97547bb001c03c8c23d46f27e351ec302a544
Chaïra Joel <79043671+Chaira-Joel@users.noreply.github.com>
2021-04-18 16:48:32 +0200

    Add files via upload

---
+++ Opdracht_007/functionex.js
@@ -0,0 +1,11 @@
+const paintWall = function (itemA, itemB){
+    console.log("The " + itemA, "wall has been painted " + itemB)
+}
+
+const green="green";
+const grey="grey";
+const orange="orange";
+const north="north";
+
+paintWall(north, orange);
+
